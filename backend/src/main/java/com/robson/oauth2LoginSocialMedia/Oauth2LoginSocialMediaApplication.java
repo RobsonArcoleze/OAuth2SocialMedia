eaa31912a122dbb0ed93624c386c3e69805f235a
RobsonArcoleze <robson.arcoleze29@gmail.com>
2023-02-23 20:23:15 -0300

    Created Project

---
+++ backend/src/main/java/com/robson/oauth2LoginSocialMedia/Oauth2LoginSocialMediaApplication.java
@@ -0,0 +1,13 @@
+package com.robson.oauth2LoginSocialMedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2LoginSocialMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2LoginSocialMediaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/robson/oauth2LoginSocialMedia/Oauth2LoginSocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robson.oauth2LoginSocialMedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2LoginSocialMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
